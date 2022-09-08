4431347e1ec8b3271fb0849cae26548339d958f2
momotaun <momotaun@student.wethinkcode.co.za>
2022-09-08 16:09:52 -0700

    Added escape character

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,10 +9,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: 1 byte(s)\n
-		Size of an int: 4 byte(s)\n
-		Size of a long int: 4 byte(s)\n
-		Size of a long long long int: 8 byte(s)\n
+	printf("Size of a char: 1 byte(s)\n \
+		Size of an int: 4 byte(s)\n \
+		Size of a long int: 4 byte(s)\n \
+		Size of a long long long int: 8 byte(s)\n \
 		Size of a float: 4 byte(s)");
 
 	return (0);
