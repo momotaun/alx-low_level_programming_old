5476922e4b2c836381764c4ff7c7eee9cecd4fb6
momotaun <momotaun@student.wethinkcode.co.za>
2022-09-08 15:55:53 -0700

    Started long string program

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints the size of various types
+ * @argv: the command input
+ *
+ * Description: program that prints the size of various 
+ * types on the computer it is compiled and run on.
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)\n
+		Size of an int: 4 byte(s)\n
+		Size of a long int: 4 byte(s)\n
+		Size of a long long long int: 8 byte(s)\n
+		Size of a float: 4 byte(s)");
+
+	return (0);
+}
