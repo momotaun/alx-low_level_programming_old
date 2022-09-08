c260fd320848b7839d185c3f584113aa0498a82c
momotaun <momotaun@student.wethinkcode.co.za>
2022-09-08 16:35:42 -0700

    CHanged the parameters

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 /**
  * main - prints the size of various types
+ * @argc: argument counter
+ * @argv: argument vector
  *
  * Description: program that prints the size of various
  * types on the computer it is compiled and run on.
  * Return: Always 0
  */
-int main(void)
+int main(int argc, char *argv[])
 {
 	printf("Size of a char: 1 byte(s)\n");
 	printf("Size of an int: 4 byte(s)\n");
-	printf("Size of a long int: 4 byte(s)\n");
+	if (argv[1] == "size32")
+		printf("Size of a long int: 4 byte(s)\n");
+	else
+		printf("Size of a long int: 8 byte(s)\n");
 	printf("Size of a long long int: 8 byte(s)\n");
 	printf("Size of a float: 4 byte(s)\n");
 
