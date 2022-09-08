cb321b7b794c97639877167b35c0e71e021f583e
momotaun <momotaun@student.wethinkcode.co.za>
2022-09-08 16:15:05 -0700

    Split the print line

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
  * main - prints the size of various types
- * @argv: the command input
  *
  * Description: program that prints the size of various 
  * types on the computer it is compiled and run on.
@@ -9,11 +8,11 @@
  */
 int main(void)
 {
-	printf("Size of a char: 1 byte(s)\n \
-		Size of an int: 4 byte(s)\n \
-		Size of a long int: 4 byte(s)\n \
-		Size of a long long long int: 8 byte(s)\n \
-		Size of a float: 4 byte(s)");
+	printf("Size of a char: 1 byte(s)\n");
+	printf("Size of an int: 4 byte(s)\n");
+	printf("Size of a long int: 4 byte(s)\n");
+	printf("Size of a long long long int: 8 byte(s)\n");
+	printf("Size of a float: 4 byte(s)");
 
 	return (0);
 }
