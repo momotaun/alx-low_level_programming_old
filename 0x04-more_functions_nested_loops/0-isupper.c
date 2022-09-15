db4da6fb23dafb34e3898ee2bf4b203946e1c98f
momotaun <momotaun@student.wethinkcode.co.za>
2022-09-15 04:32:08 -0700

    Started isupper function

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+/**
+ * _isupper -  checks for uppercase character.
+ *
+ * Return: 1 if c is uppercase otherwise returns 0.
+ */
+int _isupper(int c)
+{
+	if c > 40 && c < 91
+		return (1);
+	return (0)
+}
